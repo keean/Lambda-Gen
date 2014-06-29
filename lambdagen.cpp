10300c6eaf0aa2b4ef76a763cbe39040c54c7bc3
Keean Schupke <keean@fry-it.com>
2014-06-29 09:58:36 +0100

    parameterise syntax to allow ML code generation.

--- lambdagen.cpp
+++ lambdagen.cpp
@@ -17,6 +17,7 @@ class xor_shift_plus_128 {
 public:
     xor_shift_plus_128(xor_shift_plus_128 const&) = delete;
     xor_shift_plus_128& operator= (xor_shift_plus_128 const&) = delete;
+    
     xor_shift_plus_128() {
         uint64_t random_seed[2];
         std::ifstream random_fs;
@@ -29,10 +30,12 @@ public:
         s0 = random_seed[0];
         s1 = random_seed[1];
     }
+
     explicit xor_shift_plus_128(uint64_t const seed[2])
-        : s0(seed[0])
-        , s1(seed[1])
-        {}
+    : s0(seed[0])
+    , s1(seed[1])
+    {}
+
     uint64_t operator() () {
         uint64_t l1 = s0;
         uint64_t const l0 = s1;
@@ -48,29 +51,56 @@ double rand_01(Random& r) {
 }
 
 template <typename Random>
-void unrank(Random& r, int const i) {
+class lambdagen {
     static double constexpr p_var = 0.3703026;
     static double constexpr p_abs = 0.25939476;
-    double const n = rand_01(r);
 
-    if (i > 10000 || (i > 0 && n < p_var)) {
-        cout << "t" << r() % i;
-    } else if (n < p_var + p_abs) {
-        cout << "(\\t" << i << " . ";
-        unrank(r, i + 1);
-        cout << ")";
-    } else {
-        cout << "(";
-        unrank(r, i);
-        cout << " ";
-        unrank(r, i);
-        cout << ")";
+    Random& prng;
+    string abs1;
+    string abs2;
+
+    void unrank(int const i) {
+        double const n = rand_01(prng);
+
+        if (i > 10000 || (i > 0 && n < p_var)) {
+            cout << "t" << prng() % i;
+        } else if (n < p_var + p_abs) {
+            cout << "(" << abs1 << i << abs2;
+            unrank(i + 1);
+            cout << ")";
+        } else {
+            cout << "(";
+            unrank(i);
+            cout << " ";
+            unrank(i);
+            cout << ")";
+        }
     }
+
+public:
+    template <typename Str1, typename Str2>
+    lambdagen(Random& r, Str1&& a1, Str2&& a2)
+    : prng(r)
+    , abs1(forward<Str1>(a1))
+    , abs2(forward<Str2>(a2))
+    {}
+
+    void operator() () {
+        unrank(0);
+    }
+};
+
+template <typename Random, typename Str1, typename Str2>
+lambdagen<Random> make_lambdagen(Random& r, Str1&& a1, Str2&& a2) {
+    return lambdagen<Random>(r, forward<Str1>(a1), forward<Str2>(a2));
 }
 
 int main() {
     xor_shift_plus_128 xsp128;
+    //auto gen = make_lambdagen(xsp128, "\\t", " . ");
+    auto gen = make_lambdagen(xsp128, "fun t", " -> ");
 
-    unrank(xsp128, 0);
-    cout << "\n";
+    cout << "let t = ";
+    gen();
+    cout << ";;\n";
 }
